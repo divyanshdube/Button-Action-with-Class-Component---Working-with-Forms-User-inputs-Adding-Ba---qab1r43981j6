0ef97fb1b637ba170f28114f4ef925a48d941c7e
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-09-24 23:29:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.onClickHandler = this.onClickHandler.bind();
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  onClickHandler() {
+    <p id="para">
+      Hello, I've learnt to use the full-stack evaluation tool. This makes me so
+      happy
+    </p>;
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={this.onClickHandler}>
+          Click Me !
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
